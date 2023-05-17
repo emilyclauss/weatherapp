0215a4584bc6c0134b27a93b5d8c86db589b5876
Clauss <e136826@aps.edu>
2023-05-17 14:01:43 -0600

    replace key with env variable

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $('#get-weather').on('submit', function(event){
     city = $('#city').val();
     axios.get('https://api.weatherapi.com/v1/current.json', {
         params: {
-            'key': 'c503c597bd9b48b0964191101232604',    
+            'key': process.env.API_KEY,    
             'q': city
         }
     }).then(function(response){
